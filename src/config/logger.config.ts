ea14e7260fac9e4b3f1026a84bbb772faa97c37c
Diego-Parada-Cuervo <diego.parada@pragma.com.co>
2026-05-12 16:06:15 -0500

    feat: add src/config/logger.config.ts

---
+++ src/config/logger.config.ts
@@ -0,0 +1,14 @@
+import * as winston from 'winston';
+
+export const loggerConfig = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        winston.format.printf(({ timestamp, level, message }) => `${timestamp} [${level}]: ${message}`),
+      ),
+    }),
+  ],
+});
